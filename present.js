5b9cb2f0cb789dc2c437acfeda19ab35186e093b
gidockimberly <gidockimberly@gmail.com>
2023-01-10 13:55:57 +0800

    init

--- present.js
+++
@@ -1,7 +0,0 @@
-const fs = require ('fs');
-
-const present = function(note) {
-    let nNote = JSON.parse(note);
-
-    console.log(nNote);
-}
--- write.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const write = function(data="default"){
-    fs.writeFileSync('data.txt', data);
-}
-
-module.exports = write;
